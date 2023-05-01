14980b27cfe73a5633017ce1557123a86d6b1d24
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-01 22:48:37 +0500

    Add styling and complete feature

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// let input = document.querySelector("#input");
+let input = document.querySelector("#input");
 
 const definition = document.querySelector(".definition");
-const submit = document.querySelector(".submit");
 const api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 async function getapi() {
@@ -9,56 +8,71 @@ async function getapi() {
   let inputValue = input.value;
   const response = await fetch(`${api_url}${inputValue}`);
   const word = await response.json();
-  definition.innerHTML = `
-    <div>
-      <i>${word[0].meanings[0].partOfSpeech}</i>. 
-      ${word[0].meanings[0].definitions[0].definition}
+
+  definition.innerHTML = word[0]
+    ? `
+  <div>
+
+  <div>
+  ${
+    word[0].phonetics[0].audio
+      ? `
+      <div class="volume">
+        <h2>${inputValue}</h1>
+        <i class="fa fa-volume-up fa-2x" id="volume" onclick="play()"></i>
+      </div>
+      <audio id="audio" hidden>
+        <source src="${word[0].phonetics[0].audio}" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>`
+      : `<i>No pronunciation</i>`
+  }
+  </div>
+
+    <i>${word[0].meanings[0].partOfSpeech}</i>. 
+    ${word[0].meanings[0].definitions[0].definition}
+    <i>Example: ${
+      word[0].meanings[0].definitions[0].example
+        ? word[0].meanings[0].definitions[0].example
+        : `No example`
+    }</i>
+
+    <br><br>
+    
+    <i>${word[0].meanings[1].partOfSpeech}</i>. 
+    ${word[0].meanings[1].definitions[0].definition}
+    <i>Example: ${
+      word[0].meanings[1].definitions[0].example
+        ? word[0].meanings[1].definitions[0].example
+        : `No example`
+    }
+    </i>
+
+    <br>
+
+    ${
+      word[0].meanings[2]
+        ? `<i>${word[0].meanings[2].partOfSpeech}</i>.
+        ${word[0].meanings[2].definitions[0].definition}
       <i>Example: ${
-        word[0].meanings[0].definitions[0].example
-          ? word[0].meanings[0].definitions[0].example
+        word[0].meanings[2].definitions[0].example
+          ? word[0].meanings[2].definitions[0].example
           : `No example`
       }</i>
-
-      <br>
-      
-      <i>${word[0].meanings[1].partOfSpeech}</i>. 
-      ${word[0].meanings[1].definitions[0].definition}
-      <i>Example: ${
-        word[0].meanings[1].definitions[0].example
-          ? word[0].meanings[1].definitions[0].example
-          : `No example`
-      }
-      </i>
-
-      <br>
-
-      ${
-        word[0].meanings[2]
-          ? `<i>${word[0].meanings[2].partOfSpeech}</i>.
-          ${word[0].meanings[2].definitions[0].definition}
-        <i>Example: ${
-          word[0].meanings[2].definitions[0].example
-            ? word[0].meanings[2].definitions[0].example
-            : `No example`
-        }</i>
-        `
-          : ""
-      }
-
-    </div>
-    <div>Pronunciation:
-    ${
-      word[0].phonetics[0].audio
-        ? `<audio controls>
-    <source src="${word[0].phonetics[0].audio}" type="audio/mpeg">
-    Your browser does not support the audio element.
-  </audio>`
-        : `No pronunciation sorry`
+      `
+        : ""
     }
-    </div>
-  `;
+
+  </div>
+  
+`
+    : "Word not found";
 }
 
-submit.addEventListener("click", () => {
+input.addEventListener("input", () => {
   getapi();
 });
+
+function play() {
+  document.getElementById("audio").play();
+}
